2f3ea76bfa5d44fb26ac6b4b97b32839db371bc0
Becky <bjentwistle@gmail.com>
2023-05-15 11:01:07 +0100

    initial commit of day1 JS

---
+++ 01_dom_intro/dom_intro_start/js/app.js
@@ -0,0 +1,31 @@
+console.log('Hello World')
+
+document.addEventListener("DOMContentLoaded",() => {
+    const title = document.querySelector('h1') //selects the h1 in css
+    console.log("title", title)
+    title.textContent= "JS says Hello Becky!"
+
+    const welcome = document.querySelector("#welcome-paragraph")
+    //selecting the id of welcome-paragraph - use #
+    console.dir("id welcome-paragraph", welcome)
+    welcome.textContent = "What are you doing here? Get out!"
+
+    const allLis = document.querySelectorAll("li")
+    console.log(allLis)
+
+    const redListItem = document.querySelector("li.red")
+    console.log(redListItem)
+    redListItem.textContent = "RED!"
+    redListItem.classList.add("bold") //adds bold to the red css class itme
+
+    //mkae a new li item
+    const newListItem = document.createElement("li")
+    newListItem.textContent = "Purple"
+    newListItem.classList.add("purple")
+    console.log(newListItem)
+    //append it to its parent
+    const list = document.querySelector("ul")
+    list.appendChild(newListItem)
+
+}) //waits until the page is loaded then do the next steps inside the curly bois {}
+
